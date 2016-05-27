78b0f4d24186498c0468bbd7b4fa4b6a8510a9a1
rayapati <ravi.rayapati@in.ibm.com>
2016-05-27 15:52:00 +0530

    hhh

--- continuous-delivery-demo/src/main/java/com/ibm/cd/demo/service/MessageService.java
+++ continuous-delivery-demo/src/main/java/com/ibm/cd/demo/service/MessageService.java
@@ -2,7 +2,7 @@ package com.ibm.cd.demo.service;
 
 public class MessageService {
     public String getMsg(){
-        return "Hello World !! Spring RestController!!!!!";
+        return "Hello World !! Spring RestController!!!!!!";
     }
 
 }
